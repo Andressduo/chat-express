84f8d3060206ebcb123b0878aa68294624ecf92e
Andressduo <64800511+Andressduo@users.noreply.github.com>
2022-11-23 15:25:01 -0400

    Update database.js

--- src/database/database.js
+++ src/database/database.js
@@ -7,6 +7,17 @@ const db = new Sequelize({
     username : config.db.username,
     password : config.db.password,
     database : config.db.dbName
+    
+    /* estos es para hacer la conexion con un DB de producciion */
+    dialectOptions: 
+        process.env.NODE_ENV === 'production'
+            ? {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false
+                }
+            } : {}
+    
 })
 
 module.exports = db
